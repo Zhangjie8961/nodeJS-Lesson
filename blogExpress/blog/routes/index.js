aed74d37f6262a618ea856d38a47220c199c36eb
Zhangjie8961 <896180039@qq.com>
2019-11-12 18:35:53 +0800

    blog

--- blogExpress/blog/routes/index.js
+++ blogExpress/blog/routes/index.js
@@ -1,13 +1,21 @@
 var express = require('express');
 var router = express.Router();
-
+var {users,chapterList} = require('../views/data.json');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/login', function(req, res, next) {
+  res.render('login');
 });
 
-router.get('/login', function(req, res, next) {
-  res.render('login', { title: 'Express' });
+router.post('/login', function(req, res,next) {
+  console.log(req.body);
+  for(var i =0;i<users.length;i++){
+  if (req.body.username == users[i].username &&req.body.pwd == users[i].password ) {
+    res.render('list',{chapterList:chapterList});
+  } else {
+    res.render('error',{message:'用户密码错误'});
+    
+}
+  }
 });
 
 module.exports = router;
